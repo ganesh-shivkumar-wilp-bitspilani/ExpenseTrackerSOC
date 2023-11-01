914a3bf705687c067c7ac1e78d10fcc4946014ef
Ganesh Shivkumar <2022MT93133@wilp.bits-pilani.ac.in>
2023-11-01 16:25:30 +0530

    Adding user schema, model and controller f

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const router = express.Router();
 const {getExpense, getExpenses, createExpense, updateExpense, deleteExpense} = require("../controllers/expenseControllers")
 
+const router = express.Router();
 router.route('/').get(getExpenses).post(createExpense);
 router.route('/:id').get(getExpense).put(updateExpense).delete(deleteExpense);
 
