18c16d80498789ca7881ea5a6d407cba470beec1
Ganesh Shivkumar <2022MT93133@wilp.bits-pilani.ac.in>
2023-11-01 12:28:04 +0530

    Adding error handler middle ware + async handler

--- controllers/expenseControllers.js
+++ controllers/expenseControllers.js
@@ -1,39 +1,43 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all the expenses
 //@route GET /api/expenses
 //@access PUBLIC
-
-const getExpenses = (req, res) => {
+const getExpenses = asyncHandler(async (req, res) => {
     res.status(200).json({message : "Getting all the expenses"});
-}
+});
 
 //@desc Creating an expenses
 //@route POST /api/expenses
 //@access PUBLIC
-
-const createExpense = (req, res) => {
+const createExpense = asyncHandler(async (req, res) => {
+    const {name, amount, mode, date, description} = req.body;
+    if(!name || !amount || !mode || !date) {
+        res.status(400);
+        throw new Error("Name, Amount, Date and Mode are mandatory for creating an expense");
+    }
     res.status(201).json({message : "Creating an expenses"});
-}
+});
 
 //@desc Get the expense by id
 //@route GET /api/expenses/:id
 //@access PUBLIC
-const getExpense = (req, res) => {
+const getExpense = asyncHandler(async (req, res) => {
     res.status(200).json({message : `Getting an expenses for ${req.params.id}`});
-};
+});
 
 //@desc Update the expense by id
 //@route PUT /api/expenses/:id
 //@access PUBLIC
-const updateExpense = (req, res) => {
+const updateExpense = asyncHandler(async (req, res) => {
     res.status(200).json({message : `Updating an expenses for ${req.params.id}`});
-};
+});
 
 //@desc Delete the expense by id
 //@route DELETE /api/expenses/:id
 //@access PUBLIC
-const deleteExpense = (req, res) => {
+const deleteExpense = asyncHandler(async (req, res) => {
     res.status(200).json({message : `Deleting an expenses for ${req.params.id}`});
-};
-
+});
 
 module.exports = {getExpenses, createExpense, getExpense, updateExpense, deleteExpense};
