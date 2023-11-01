b9540854b4aebc5680f3d8e4a82b2e3feb8a3fb6
Ganesh Shivkumar <2022MT93133@wilp.bits-pilani.ac.in>
2023-11-01 17:30:54 +0530

    Adding access controls for the methods

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const {getExpense, getExpenses, createExpense, updateExpense, deleteExpense} = require("../controllers/expenseControllers")
+const {getExpense, getExpenses, createExpense, updateExpense, deleteExpense} = require("../controllers/expenseControllers");
+const validateToken = require("../middleware/validateToken");
 
 const router = express.Router();
+router.use(validateToken);
 router.route('/').get(getExpenses).post(createExpense);
 router.route('/:id').get(getExpense).put(updateExpense).delete(deleteExpense);
 
